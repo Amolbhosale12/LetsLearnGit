d71946e9b9310f93324892635694b05eee6df8bd
Amolbhosale12 <119960300+Amolbhosale12@users.noreply.github.com>
2023-01-18 23:17:13 +0530

    Delete Substraction.java

--- Substraction.java
+++
@@ -1,8 +0,0 @@
-public class Substraction {
-    public static void main(String x[]){
-        int a=45;
-        int b=34;
-        System.out.println("Substraction of number is:" + (a-b));
-    }
-    
-}
