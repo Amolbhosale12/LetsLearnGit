cf2bfcc2e65bfabc354636b078ade363e13ff0ed
Amol Bhosale <ambhosale2022@gmail.com>
2023-01-18 18:20:11 +0530

    initial take input from user.

--- Tables.java
+++ Tables.java
@@ -1,8 +1,13 @@
+// take input from user and print table whatever they want.
+import java.util.*;
 public class Tables {
     public static void main(String x[]){
-    int a=2;
+        Scanner sc=new Scanner(System.in);
+        int num;
+        System.out.println("enter number here:");
+        num=sc.nextInt();
       for(int i=1;i<=10;i++){
-        System.out.println(a*1);
+        System.out.println(num*1);
       }
     }
 }
