c797e63f03caaa44cb6bd57aee1d9926dc8c5b71
Amol Bhosale <ambhosale2022@gmail.com>
2023-01-18 22:49:22 +0530

    initial stage

---
+++ AllOperation.java
@@ -0,0 +1,11 @@
+public class AllOperation {
+    public static void main(String []args){
+         int x=10;
+         int y=10;
+         System.out.println(x+y);
+         System.out.println(x*y);
+         System.out.println(x-y);
+         System.out.println(x/y);
+         
+    }
+}
