9a9a64f473fa5c3bc0a776c987eed6b101cff6fd
Amol Bhosale <ambhosale2022@gmail.com>
2023-01-18 16:45:24 +0530

    initial commit

---
+++ Substraction.java
@@ -0,0 +1,8 @@
+public class Substraction {
+    public static void main(String x[]){
+        int a=45;
+        int b=34;
+        System.out.println("Substraction of number is:" + (a-b));
+    }
+    
+}
---
+++ Sum.java
@@ -0,0 +1,8 @@
+public class Sum{
+    public static void main(String x[]){
+        int a,b;
+        a=2;
+        b=4;
+        System.out.println("Sum of two number is:"+a+b);
+    }
+}
